f44bb58771c0d6610690856bdb19c7d89fdd826f
khonsothearit <khonsothearitkh@gmail.com>
2016-01-08 10:33:21 +0700

    Revert "Revert "connect and layout""
    This reverts commit 95fbef37386f4191414e164e616db99346319913.

---
+++ app/Collection/Product.php
@@ -0,0 +1,8 @@
+<?php namespace App\Collection;
+use Jenssegers\Mongodb\Model as Eloquent;
+
+class Product extends Eloquent {
+
+    protected $collection = 'product';
+
+}
---
+++ app/Http/Controllers/User/FormController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Request;
+use App\Http\Controllers\Controller;
+use App\Collection\Product;
+use Validator;
+class FormController extends Controller
+{
+
+}
